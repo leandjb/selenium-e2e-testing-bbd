c232a1800e0ddee784b3e0c1935161fd32808370
leandjb <leandjb@gmail.com>
2024-11-10 14:50:46 -0500

    "updated selenium 4.26 version

---
+++ app/src/test/java/org/example/GoogleSearchTest.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GoogleSearchTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+        assertTrue(driver.getTitle().contains("Selenium WebDriver"));
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
--- app/src/test/resources/features/logjn.feature
+++
@@ -1,6 +0,0 @@
-Feature: Do login to FreeRangeTesters page
-
-Scenario:
-    Given i navigate to home page
-    When i enter a valid credential
-    Then login is success
